dfce213620fd4824690317c21269e7a254422620
Viacheslav Azarov <public@religiosa.ru>
2024-06-14 13:53:24 +0200

    env example

--- server/src/middleware/authenticate.ts
+++ server/src/middleware/authenticate.ts
@@ -2,12 +2,12 @@ import { createMiddleware } from "hono/factory";
 import { basicAuth as honoBasicAuth } from "hono/basic-auth";
 
 export const authenticate = createMiddleware(async (c, next) => {
-  if (!process.env.AUTH_USER || !process.env.AUTH_PASSWORD) {
+  if (!process.env.AUTH_USER || !process.env.AUTH_PASS) {
     return next();
   }
   const mw = honoBasicAuth({
     username: process.env.AUTH_USER,
-    password: process.env.AUTH_PASSWORD,
+    password: process.env.AUTH_PASS,
   });
   return mw(c, next);
 });
