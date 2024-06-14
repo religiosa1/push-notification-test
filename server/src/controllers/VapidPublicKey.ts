5f4386acc84cf618f917b6a769cc7db12f04f235
Viacheslav Azarov <public@religiosa.ru>
2024-06-14 17:22:51 +0200

    Cors for VapidPublicKey

--- server/src/controllers/VapidPublicKey.ts
+++ server/src/controllers/VapidPublicKey.ts
@@ -1,9 +1,10 @@
 import { Hono } from "hono";
 import { HTTPException } from "hono/http-exception";
+import { clientCors } from "../middleware/clientCors";
 
 const router = new Hono();
 
-router.get("/", (c) => {
+router.get("/", clientCors, (c) => {
   const key = process.env.VAPID_PUBLIC_KEY;
   if (!key) {
     throw new HTTPException(500, {
--- server/src/middleware/clientCors.ts
+++ server/src/middleware/clientCors.ts
@@ -5,6 +5,9 @@ export const clientCors = createMiddleware(async (c, next) => {
   if (!process.env.CLIENT_URL) {
     return next();
   }
-  const mw = cors({ origin: process.env.CLIENT_URL });
+  const mw = cors({
+    origin: process.env.CLIENT_URL,
+    allowMethods: ["POST", "GET"],
+  });
   return mw(c, next);
 });
