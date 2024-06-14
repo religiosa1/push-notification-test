7ffb5c98d72727887a47efdc178093919c941811
Viacheslav Azarov <public@religiosa.ru>
2024-06-14 13:57:32 +0200

    server port in env

--- server/src/index.ts
+++ server/src/index.ts
@@ -26,7 +26,7 @@ app.get("/vapidPublicKey", (c) => {
 
 console.log("Running migrations...");
 migrate().then(() => {
-  const port = 3000;
+  const port = parseInt(process.env.PORT ?? "", 10) || 3000;
   console.log(`Server is running on port ${port}`);
 
   serve({
