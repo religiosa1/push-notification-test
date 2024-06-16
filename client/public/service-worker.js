ea294f2ddac4409d4fda4666c2c0162f6e48353b
Viacheslav Azarov <public@religiosa.ru>
2024-06-16 21:54:16 +0200

    Open some url on click on notification

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -1,5 +1,3 @@
-console.log("hello from service worker");
-
 self.addEventListener("push", function (event) {
 	const data = event.data?.json();
 	if (!data || typeof data !== "object") {
@@ -19,4 +17,28 @@ self.addEventListener("push", function (event) {
 	// Keep the service worker alive until the notification is created.
 	// https://developer.mozilla.org/en-US/docs/Web/API/ExtendableEvent/waitUntil
 	event.waitUntil(notificationPromise);
+});
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Clients/openWindow#examples
+// Notification click event listener
+self.addEventListener("notificationclick", (e) => {
+	const TARGET_URL = "https://push-client.religiosa.ru/";
+	e.notification.close();
+	e.waitUntil(
+		// Get all the Window clients
+		clients.matchAll({ type: "window" }).then((clientsArr) => {
+			// If a Window tab matching the targeted URL already exists, focus that;
+			const hadWindowToFocus = clientsArr.some((windowClient) =>
+				windowClient.url === TARGET_URL // e.notification.data.url
+					? (windowClient.focus(), true)
+					: false
+			);
+			// Otherwise, open a new tab to the applicable URL and focus it.
+			if (!hadWindowToFocus) {
+				clients
+					.openWindow(TARGET_URL)
+					.then((windowClient) => (windowClient ? windowClient.focus() : null));
+			}
+		})
+	);
 });
