e0ef683bc7a11c072875bbd0a3ed6504e9d1bd95
Viacheslav Azarov <public@religiosa.ru>
2024-06-17 00:30:15 +0200

    Service worker bugfixes + url from notificatoin tag

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -3,7 +3,6 @@ self.addEventListener("push", function (event) {
 	if (!data || typeof data !== "object") {
 		console.log("no data");
 	}
-	console.log(data);
 
 	const title =
 		typeof data.title === "string" && data.title
@@ -13,19 +12,26 @@ self.addEventListener("push", function (event) {
 		typeof data.body === "string" && data.body ? data.body : "Empty body";
 	const notificationPromise = self.registration.showNotification(title, {
 		body: body,
+		tag: "https://push-client.religiosa.ru/#someTestTag",
 	});
 	// Keep the service worker alive until the notification is created.
 	// https://developer.mozilla.org/en-US/docs/Web/API/ExtendableEvent/waitUntil
 	event.waitUntil(notificationPromise);
 });
 
-// https://developer.mozilla.org/en-US/docs/Web/API/Clients/openWindow#examples
 // Notification click event listener
-self.addEventListener("notificationclick", (e) => {
-	e.notification.close();
-	e.waitUntil(openAndFocusUrl("https://push-client.religiosa.ru/"));
+self.addEventListener("notificationclick", function (event) {
+	event.notification.close();
+	// There's a e.notification.data.url, but it's not supported on safari/IOS
+	// https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerRegistration/showNotification#browser_compatibility
+	// We can try to hijack Notification's tag property, if we need to dynamically
+	// determine what kind of URL do we want to show.
+	// https://developer.mozilla.org/en-US/docs/Web/API/Notification/tag
+	const url = event.notification.tag || "https://push-client.religiosa.ru/";
+	event.waitUntil(openAndFocusUrl(url));
 });
 
+// https://developer.mozilla.org/en-US/docs/Web/API/Clients/openWindow#examples
 async function openAndFocusUrl(url) {
 	const windowClients = await clients.matchAll({ type: "window" });
 	const windowTabToFocus = windowClients.find((client) => client.url === url);
@@ -34,9 +40,11 @@ async function openAndFocusUrl(url) {
 		windowTabToFocus.focus();
 	} else {
 		// Otherwise, open a new tab to the applicable URL and focus it.
-		const client = await windowClients.openWindow(url);
+		const client = await clients.openWindow(url);
 		if (client) {
-			client.focus();
+			client.focus().catch((err) => {
+				console.log("Unable to focus the window:", err);
+			});
 		}
 	}
 }
