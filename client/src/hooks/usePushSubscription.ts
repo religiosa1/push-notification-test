c6e2806b12dede32044caea5b96ee73e48a973e6
Viacheslav Azarov <public@religiosa.ru>
2024-06-16 21:41:19 +0200

    Autoupdate subscription

--- client/src/hooks/usePushSubscription.ts
+++ client/src/hooks/usePushSubscription.ts
@@ -1,11 +1,17 @@
 import { createResource } from "solid-js";
 import { withTimeout } from "../utils/withTimeout";
+import { sendSubscriptionToServer } from "../api";
 
 export function usePushSubscription() {
 	const [foo] = createResource(() =>
 		withTimeout(async () => {
 			const registration = await navigator.serviceWorker.ready;
 			const subscription = await registration.pushManager.getSubscription();
+			// If we have an active subscription, we're sending it to backend immediately,
+			// to update and sync data just in case.
+			if (subscription != null) {
+				await sendSubscriptionToServer(subscription);
+			}
 			return { registration, subscription };
 		}),
 	);
