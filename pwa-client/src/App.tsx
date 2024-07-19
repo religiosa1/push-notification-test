a03d8edd1ccd1b27a3f91bb4f639555153c4f8d4
Viacheslav Azarov <public@religiosa.ru>
2024-07-19 16:08:07 +0200

    PWA as a wrapper aound original app

--- pwa-client/src/App.tsx
+++ pwa-client/src/App.tsx
@@ -1,15 +1,27 @@
-import { ErrorBoundary } from "solid-js";
+import { createSignal, ErrorBoundary, Suspense } from "solid-js";
 import PWABadge from "./PWABadge.tsx";
-
-import { ErrorDisplay } from "./ErrorDisplay.tsx";
-import "./App.css";
+import { usePushSubscription } from "@client/hooks/usePushSubscription";
+import { SubUnsub } from "@client/SubUnsub";
+import { ErrorDisplay } from "@client/ErrorDisplay.tsx";
 
 function App() {
+  const [subAndReg, stage] = usePushSubscription();
+  const [changedSub, setChangedSub] = createSignal<PushSubscription | null>();
+
+  const subscription = () =>
+    changedSub() !== undefined ? changedSub() : subAndReg()?.subscription;
+
   return (
     <ErrorBoundary
       fallback={(error, reset) => <ErrorDisplay error={error} reset={reset} />}
     >
-      <p>Here comes our actual app</p>
+      <Suspense fallback={<span>Loading {stage()}...</span>}>
+        <SubUnsub
+          registration={subAndReg()?.registration!}
+          subscription={subscription() ?? null}
+          onSubscripionChange={setChangedSub}
+        />
+      </Suspense>
       <PWABadge />
     </ErrorBoundary>
   );
